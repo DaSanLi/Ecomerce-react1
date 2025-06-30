7736485118aaf5bf635addd58ef1640c05abfce4
danos <d.linares583@gmail.com>
2025-06-30 18:12:36 +0100

    R-L 6

--- src/componentes/Registro.jsx
+++ src/componentes/Registro.jsx
@@ -20,7 +20,7 @@ const handleSubmit = async (e) =>{
 
     try{
 
-        const response = await fetch('http://localhost/tareasPHP/API_REST/Registro.php', {
+        const response = await fetch('https://ecomerce.is-great.net/Registro.php', {
         method: 'POST',
         headers: {
                     'Content-Type': 'application/json', // Indica que se envía JSON  
